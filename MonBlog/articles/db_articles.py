6f22445f15b1eb6be5ef6610aad2ba26b8c64a5c
NickyLarcinFr <87197065+NickyLarcinFr@users.noreply.github.com>
2021-07-24 23:24:59 +0200

    Website Python Online

---
+++ MonBlog/articles/db_articles.py
@@ -0,0 +1,17 @@
+articles = [
+    {
+        "titre": "Premier article",
+        "date_publication": "01/02/2021",
+        "contenu": "Contenu du prermier article"
+    },
+    {
+        "titre": "Deuxième article",
+        "date_publication": "02/02/2021",
+        "contenu": "Contenu du deuxième article"
+    },
+    {
+        "titre": "Troisième article",
+        "date_publication": "03/02/2021",
+        "contenu": "Contenu du troisième article"
+    },
+]
